dbf8a56f115aa428b5d88719010784b74d0ab583
Le Hai Duong <duonghaile@gmail.com>
2017-03-22 22:39:46 +0700

    change in mail again 1

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,6 +1,14 @@
-<h1>Dear {{ $name }}</h1>
-<p>Sending Mail from Laravel.</p>
-<?php 
-    echo Hash::make($name.$salt);
-    echo "This is ".$name." ".$salt;
-?>
+<!DOCTYPE html>
+<html lang="vi" prefix="og: http://ogp.me/ns#">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="http://tuyensinh.hcmiu.edu.vn/xmlrpc.php" />
+<title>Đăng ký dự thi kỳ thi kiểm tra năng lực - Trường Đại học Quốc Tế - Tuyển sinh đại học năm 2017</title>	
+	
+<header class="entry-header">
+               <h1 class="entry-title">
+            Đăng ký dự thi kỳ thi kiểm tra năng lực         </h1>
+</header>
+<p><strong>I. THÔNG TIN THÍ SINH</strong><br />
